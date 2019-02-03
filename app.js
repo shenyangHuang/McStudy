aaa3116f14a55046abacd3aa95537d88cf683206
Andy <shenyang.huang@mail.mcgill.ca>
2019-02-02 20:50:09 -0500

    Ready for Backend update

--- app.js
+++ app.js
@@ -1,7 +1,74 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var app = express();
 
+//db section
+//Hosted mongoose database
+mongoose.connect('mongodb://Andy:Mchacks2019@ds119755.mlab.com:19755/mcstudy');
+
+//defining schema for a user
+var profileSchema = new mongoose.Schema({
+	UID: Number,
+	Name: String,
+	age: Number,
+	friends: Array,
+})
+
+var Profile = mongoose.model("Profile", profileSchema);
+
+var andy = new Profile({
+	UID: "1",
+	Name: "Andy",
+	age: 20,
+	friends: ["Yunxi", "Dylan"]
+});
+
+// andy.save(function(err, profile){
+// 	if(err){
+// 		console.log("Error Encountered!");
+// 	} else {
+// 		console.log("Saving profile to DB");
+// 		console.log(profile);
+// 	}
+// });
+
+
+//create function
+Profile.create({
+	UID: "1",
+	Name: "Andy",
+	age: 20,
+	friends: ["Yunxi", "Dylan"]
+}, function(err, profile){
+	if(err){
+		console.log(err);
+	} else {
+		console.log(profile);
+	}
+});
+
+
+//retrieve all info from database
+Profile.find({}, function(err, profiles){
+	if(err){
+		console.log(err);
+	} else {
+		console.log()
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+//routing section
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -19,7 +86,7 @@ app.get("/", function(req, res){
 app.post("/addfriend", function(req, res){
 	var newFriend = req.body.newfriend;
 	friends.push(newFriend);
-	res.send("You are trying to post");
+	res.redirect("/friends");
 });
 
 
@@ -44,6 +111,6 @@ app.get("*", function(req,res){
 
 // 3000 is the set port number
 app.listen(8100, process.env.IP, function(){
-	console.log("listening service has started");
+	console.log("McStudy server has started");
 });
 
